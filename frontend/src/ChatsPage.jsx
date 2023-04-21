62302da6e5802c555c2d7ca9b846a994b3ac6874
gh0sty <nice140@icloud.com>
2023-04-21 12:53:59 +0300

    1.0

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,11 +1,23 @@
-import {MultiChatSocket,MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
-
+//import {MultiChatSocket,MultiChatWindow, useMultiChatLogic} from 'react-chat-engine-advanced'
+import {PrettyChatWindow} from 'react-chat-engine-pretty'
 const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic('c9f587b4-7ff5-4c49-bac8-7e068547dadb',props.user.username, props.user.secret);
-    return(<div style={{height: '100vh'}}>
-        <MultiChatSocket {...chatProps}/>
-        <MultiChatWindow {...chatProps} style={{height : '100%'}}/>
-    </div>
+    return (
+    // const chatProps = useMultiChatLogic('c9f587b4-7ff5-4c49-bac8-7e068547dadb',props.user.username, props.user.secret);
+    // return(<div style={{height: '100vh'}}>
+    //     <MultiChatSocket {...chatProps}/>
+    //     <MultiChatWindow {...chatProps} style={{height : '100%'}}/>
+    // </div>
+    // )
+
+    <div style={{height:'100vh'}}>
+        <PrettyChatWindow
+            projectId='c9f587b4-7ff5-4c49-bac8-7e068547dadb'
+            username={props.user.username}
+            secret={props.user.secret}
+            style={{height:'100%'}}
+            />
+       </div>
+    
     )
 }
 
